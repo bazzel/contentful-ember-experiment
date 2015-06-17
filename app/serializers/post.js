9f433410cc80c54e7a92678c3ac1083a6536922a
Patrick Baselier <patrick@kabisa.nl>
2015-06-17 11:49:26 +0200

    Use proper id

--- app/serializers/post.js
+++ app/serializers/post.js
@@ -2,18 +2,15 @@ import DS from 'ember-data';
 
 export default DS.RESTSerializer.extend({
   extractArray: function(store, type, payload) {
-    var posts = payload.items.map(function(post) {
-      return post.fields;
+    var posts = payload.items.map(function(item) {
+      var post = Ember.$.extend(true, {}, item.fields);
+      post.id = item.sys.id;
+      return post;
     });
 
     payload = {};
     payload[type.modelName] = posts;
 
     return this._super(store, type, payload);
-  },
-  normalize: function(typeClass, hash, prop) {
-    hash.id = hash.slug;
-
-    return this._super(typeClass, hash, prop);
   }
 });
