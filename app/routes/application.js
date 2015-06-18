b8dd1840fcee6ae67f88e1d933ba3ff1b2eb4fd8
Patrick Baselier <patrick@kabisa.nl>
2015-06-18 23:28:41 +0200

    Get categories from contentful

--- app/routes/application.js
+++ app/routes/application.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model: function() {
     return Ember.RSVP.hash({
-      categories: [{id: 1, name: 'Foo'}, {id: 2, name: 'Bar'}]
+      categories: this.store.find('category')
     });
   },
   setupController: function(controller, model) {
---
+++ app/serializers/category.js
@@ -0,0 +1,16 @@
+import DS from 'ember-data';
+
+export default DS.RESTSerializer.extend({
+  extractArray: function(store, type, payload) {
+    var categories = payload.items.map(function(item) {
+      var category = Ember.$.extend(true, {}, item.fields);
+      category.id = item.sys.id;
+      return category;
+    });
+
+    payload = {};
+    payload[type.modelName] = categories;
+
+    return this._super(store, type, payload);
+  }
+});
