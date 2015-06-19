03bf8faa6ab88a42609f38b081e643cab95eb981
Patrick Baselier <patrick@kabisa.nl>
2015-06-19 08:29:39 +0200

    Introduce ContenfulBase adapter

--- app/adapters/category.js
+++ app/adapters/category.js
@@ -1,9 +1,5 @@
-import DS from 'ember-data';
-import ajax from 'ic-ajax';
+import ContentfulBase from './contentful-base';
 
-export default DS.RESTAdapter.extend({
-  findAll: function() {
-    var url = 'https://cdn.contentful.com/spaces/k2z9tfoe7o9n/entries?content_type=5KMiN6YPvi42icqAUQMCQe&access_token=fe6eb8da16a59ce6de939cb35e2528f8848f7b505225cc1854b5a0bc0fba2aaa';
-    return ajax(url);
-  }
+export default ContentfulBase.extend({
+  contentTypeId: '5KMiN6YPvi42icqAUQMCQe'
 });
---
+++ app/adapters/contentful-base.js
@@ -0,0 +1,16 @@
+import DS from 'ember-data';
+import ajax from 'ic-ajax';
+
+export default DS.RESTAdapter.extend({
+  spaceId: 'k2z9tfoe7o9n',
+  accessToken: 'fe6eb8da16a59ce6de939cb35e2528f8848f7b505225cc1854b5a0bc0fba2aaa',
+  contentTypeId: undefined,
+  findAll: function() {
+    var url = 'https://cdn.contentful.com/spaces/' + this.spaceId + '/entries?content_type=' + this.contentTypeId + '&access_token=' + this.accessToken;
+    return ajax(url);
+  },
+  find: function(store, type, id, snapshot) {
+    var url = 'https://cdn.contentful.com/spaces/' + this.spaceId + '/entries/' + id + '?access_token=' + this.accessToken;
+    return ajax(url);
+  }
+});
