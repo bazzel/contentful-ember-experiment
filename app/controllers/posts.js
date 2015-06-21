2c5af0deb2d519790b975d5b328a2e728e34b8a2
Patrick Baselier <patrick@kabisa.nl>
2015-06-21 20:33:52 +0200

    Filter on category

--- app/controllers/posts.js
+++ app/controllers/posts.js
@@ -2,5 +2,19 @@ import Ember from 'ember';
 
 export default Ember.ArrayController.extend({
   sortProperties: ['date'],
-  sortAscending: false
+  sortAscending: false,
+  queryParams: ['category'],
+  category: null,
+  filteredPosts: function() {
+    var category = this.get('category');
+    var posts = this.get('arrangedContent');
+
+    if (category) {
+      return posts.filter(function(post) {
+        return post.get('categories').mapBy('title').contains(category);
+      });
+    } else {
+      return posts;
+    }
+  }.property('category', 'model')
 });
--- app/routes/post.js
+++ app/routes/post.js
@@ -1,16 +1,16 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  setupController: function(controller, model) {
-    this._super(controller, model);
-    this.controllerFor('categories').set('model', this.store.find('category'));
-  },
-  renderTemplate: function() {
-    this._super();
-    this.render('categories', {
-      into: 'post',
-      outlet: 'sidebar',
-      controller: 'categories'
-    });
-  }
+  //setupController: function(controller, model) {
+    //this._super(controller, model);
+    //this.controllerFor('categories').set('model', this.store.find('category'));
+  //},
+  //renderTemplate: function() {
+    //this._super();
+    //this.render('categories', {
+      //into: 'post',
+      //outlet: 'sidebar',
+      //controller: 'categories'
+    //});
+  //}
 });
