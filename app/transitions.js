6d3762742965544549d90dee52e9f18567563b13
Patrick Baselier <patrick@kabisa.nl>
2015-06-23 08:37:12 +0200

    More liquid experiments

--- app/transitions.js
+++ app/transitions.js
@@ -5,4 +5,19 @@ export default function(){
     this.use('toLeft'),
     this.reverse('toRight')
   );
+  this.transition(
+    this.childOf('.posts'),
+    this.use('explode', {
+      matchBy: 'data-post-id',      // matchBy will look for the same
+                                    // HTML attribute value in both
+                                    // the old and new elements, and
+                                    // for each matching pair, it
+                                    // runs the given transition.
+
+                                    // fly-to is a built in transition that animate the element
+                                    // moving from the position of oldElement to the position of
+                                    // newElement.
+      use: ['fly-to', { duration: 100, easing: [250, 15] }]
+    })
+  );
 };
